3bf79584389c44aa9f8a94a8cedce6caae672d50
eddy.de.boer <eddy.de.boer@medicore.nl>
2018-11-09 15:08:33 +0100

    master: Working, without the routing

--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,15 +1,22 @@
 (ns clojure-simple-http.core
-  (:require [org.httpkit.server :refer [run-server]]
-            [clj-time.core :as t])
+  (:use [org.httpkit.server :refer [run-server]]
+        [clj-time.core :as t]
+        [compojure.route :only [files not-found]]
+        [compojure.handler :only [site]]
+        [compojure.core :only [defroutes GET PUT POST DELETE ANY context]])
   (:gen-class))
 
-;@todo:
-;https://github.com/tolitius/mount/issues/72
+(defroutes all-routes
+  (GET "/" [] app)
+  (context "/user/:id" []
+           (GET / [] get-user)
+           (PUT / [] update-user))
+  (not-found "<h1>404</h2>"))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    (str "HelloWorld")})
+   :body    (str "<h1>Hello World</h1>")})
 
 (defonce server (atom nil))
 
@@ -27,4 +34,9 @@
   ;; and http://http-kit.org/migration.html#reload
   (do
     (println "Web server started at http://127.0.0.1:8080")
-    (reset! server (run-server #'app {:port 8080}))))
+    (reset! server (run-server #'all-routes {:port 8080}))))
+
+(defn get-user [req]
+  {:status   200
+   :headers  {"Content-Type" "text-html"}
+   :body     (str "<h1>Info User</h1><p>Naam: Jaap Knasterhuis</p>")})
