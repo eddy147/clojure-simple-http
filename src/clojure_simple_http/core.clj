e3db91330dd4e4b3bec339fb76b0d1deb863e726
eddy.de.boer <eddy.de.boer@medicore.nl>
2018-11-09 16:26:02 +0100

    master: working example with routing

--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -4,6 +4,8 @@
         [compojure.route :as route]
         (:gen-class)))
 
+(defonce server-is-running (atom nil))
+
 (defn fps-handler [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
@@ -28,9 +30,17 @@
   (ANY "/anything-goes" [] general-handler)
   (route/not-found "You Must Be New Here")) 
 
+(defn start-server []
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))] 
+    (reset! server-is-running (server/run-server #'app-routes {:port port}))
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
+
+(defn stop-server []
+  (when-not (nil? @server-is-running )
+    (@server-is-running :timeout 100)
+    (reset! server-is-running nil)))
+
 (defn -main
   "This is our app's entry point"
   [& args]
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))] 
-    (server/run-server #'app-routes {:port port})
-    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
+  (start-server))
