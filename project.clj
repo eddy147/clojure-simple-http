978900b5bb82b7219cd5e60cf1bda44e8c1ae5ab
eddy.de.boer <eddy.de.boer@medicore.nl>
2018-11-09 15:38:53 +0100

    master: Working with routes...

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
-                 [clj-time "0.15.0"]]
+                 [clj-time "0.15.0"]
+                 [compojure "1.6.1"]
+                 [javax.servlet/servlet-api "2.5"]]
   :main clojure-simple-http.core
   :aot [clojure-simple-http.core])
--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,42 +1,36 @@
 (ns clojure-simple-http.core
-  (:use [org.httpkit.server :refer [run-server]]
-        [clj-time.core :as t]
-        [compojure.route :only [files not-found]]
-        [compojure.handler :only [site]]
-        [compojure.core :only [defroutes GET PUT POST DELETE ANY context]])
-  (:gen-class))
+  (:use [org.httpkit.server :as server]
+        [compojure.core :refer :all]
+        [compojure.route :as route]
+        (:gen-class)))
 
-(defroutes all-routes
-  (GET "/" [] app)
-  (context "/user/:id" []
-           (GET / [] get-user)
-           (PUT / [] update-user))
-  (not-found "<h1>404</h2>"))
-
-(defn app [req]
+(defn fps-handler [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    (str "<h1>Hello World</h1>")})
+   :body    "Pew pew!"})
 
-(defonce server (atom nil))
+(defn mail-man [] 
+  "{\"Spongebob Narrator\": \"5 years later...\"}")
 
-(defn stop-server []
-  (when-not (nil? @server)
-    ;; graceful shutdown: wait 100ms for existing requests to be finished
-    ;; :timeout is optional, when no timeout, stop immediately
-    (@server :timeout 100)
-    (reset! server nil)
-    (println "Server stopped.")))
+(defn mail-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/json"}
+   :body    (mail-man)}) 
 
-(defn start-server []
-  ;; The #' is useful when you want to hot-reload code
-  ;; You may want to take a look: https://github.com/clojure/tools.namespace
-  ;; and http://http-kit.org/migration.html#reload
-  (do
-    (println "Web server started at http://127.0.0.1:8080")
-    (reset! server (run-server #'all-routes {:port 8080}))))
+(defn general-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "All hail General Zod!"})
 
-(defn get-user [req]
-  {:status   200
-   :headers  {"Content-Type" "text-html"}
-   :body     (str "<h1>Info User</h1><p>Naam: Jaap Knasterhuis</p>")})
+(defroutes app-routes 
+  (GET "/" [] fps-handler)
+  (POST "/postoffice" [] mail-handler)
+  (ANY "/anything-goes" [] general-handler)
+  (route/not-found "You Must Be New Here")) 
+
+(defn -main
+  "This is our app's entry point"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))] 
+    (server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
